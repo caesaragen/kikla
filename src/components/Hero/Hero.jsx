4b69742b0f7d798ce82ba12c07c9185050d8f5bf
Joel Aduma <caesaragen@gmail.com>
2023-05-22 15:31:40 +0300

    initialize

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from "framer-motion-carousel";
 import "./Hero.css";
 import Header from "../Header/Header";
 
@@ -6,13 +7,15 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import { image1, image2, image3, kikla, kikla1, kikla2, kikla3, kikla4, kikla5, kikla6, coaches, coaches1, player, coaches2 } from "../../data/kikla_images";
 
 import { motion } from "framer-motion";
 import NumberCounter from "number-counter";
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
   const mobile = window.innerWidth <= 768 ? true : false;
-
+  const carouselRef = React.useRef();
+  const imagesArray = [kikla1, kikla3, coaches2, coaches1, player]
   return (
     <div className="hero" id="home">
       <div className="blur hero-blur"></div>
@@ -27,24 +30,31 @@ const Hero = () => {
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
-          <span>the best fitness club in the town</span>
+          <span>
+            Developing the best sports and social institutions Internationally</span>
         </div>
 
         {/* Hero Heading */}
         <div className="hero-text">
           <div>
-            <span className="stroke-text">Shape </span>
-            <span>Your</span>
+            <span className="stroke-text">MAXIMIZE </span>
+            <span>YOUR</span>
           </div>
 
           <div>
-            <span>Ideal body</span>
+            <span> GAME TO GROW</span>
           </div>
 
           <div>
             <span>
-              In here we will help you to shape and build your ideal body and
-              live up your life to fullest
+              <p>
+                Kikla has its focus on all types of sports in nurturing talents, gifting, and developing skills at an early age.<br></br>
+                The focus is on Basketball at the moment.<br></br> 
+                This is through programs that run throughout the year with a clear mission and vision under Kikla Basketball Club.
+              </p>
+              <p>
+                However, we have already acquired necessary equipment and staff personnel in other sports like boxing, martial arts, karate, archery, badminton, Pilates, yoga, and many more to be introduced.
+              </p>
             </span>
           </div>
         </div>
@@ -53,19 +63,19 @@ const Hero = () => {
         <div className="figures">
           <div>
             <span>
-              <NumberCounter end={140} start={100} delay="4" preFix="+" />
+              <NumberCounter end={4} start={0} delay="4" preFix="+" />
             </span>
             <span>expert coaches</span>
           </div>
           <div>
             <span>
-              <NumberCounter end={978} start={800} delay="4" preFix="+" />
+              <NumberCounter end={25} start={0} delay="4" preFix="+" />
             </span>
             <span>members joined</span>
           </div>
           <div>
             <span>
-              <NumberCounter end={50} start={0} delay="4" preFix="+" />
+              <NumberCounter end={4} start={0} delay="4" preFix="+" />
             </span>
             <span>fitness programs</span>
           </div>
@@ -93,8 +103,20 @@ const Hero = () => {
         </motion.div>
 
         {/* hero images */}
-        <img src={hero_image} alt="hero_image" className="hero-image" />
-
+        {/* <img src={kikla1} alt="hero_image" className="hero-image" /> */}
+        <Carousel ref={carouselRef} autoPlay={false}>
+          {imagesArray.map((item, i) => (
+            <img
+              draggable="false"
+              src={item}
+              key={i}
+              width="100%"
+              // height="80%"
+              alt=""
+              className="carousel-img"
+            />
+          ))}
+        </Carousel>
         <motion.img
           src={hero_image_back}
           alt="hero_image_back"
---
+++ src/data/kikla_images.js
@@ -0,0 +1,18 @@
+export { default as image1 } from "../assets/kikla_images/IMG_20201222_132416_076.jpg";
+export { default as image2 } from "../assets/kikla_images/IMG_20201222_132421_410.jpg";
+export { default as image3 } from "../assets/kikla_images/IMG_20201222_132432_491.jpg";
+export { default as kikla } from "../assets/kikla_images/IMG_20201222_132456_894.jpg";
+export { default as kikla1 } from "../assets/kikla_images/IMG_20210802_112740_887.jpg";
+export { default as kikla2 } from "../assets/kikla_images/IMG_20210814_144420_301.jpg";
+export { default as kikla3 } from "../assets/kikla_images/IMG_20210814_144455_479.jpg";
+export { default as kikla4 } from "../assets/kikla_images/IMG_20210814_144458_138.jpg";
+export { default as kikla5 } from "../assets/kikla_images/IMG_20210925_105312_627.jpg";
+export { default as kikla6 } from "../assets/kikla_images/IMG_20210925_105315_203.jpg";
+export { default as kikla7 } from "../assets/kikla_images/IMG_20210925_110753_610.jpg";
+export { default as kikla8 } from "../assets/kikla_images/IMG_20210925_110757_929.jpg";
+export { default as kikla9 } from "../assets/kikla_images/IMG_20210925_110759_405.jpg";
+export { default as coaches } from "../assets/kikla_images/IMG_20210926_154514_515.jpg";
+export { default as coaches1 } from "../assets/kikla_images/IMG_20210926_154616_174.jpg";
+export { default as coaches2} from "../assets/kikla_images/IMG_20210926_154631_128.jpg";
+export { default as player } from "../assets/kikla_images/IMG_20211114_141045_894.jpg";
+
