4f2eb1025ffbeab1f35829bf8b2f52c9ed223043
Joel Aduma <caesaragen@gmail.com>
2023-05-22 17:43:13 +0300

    customize crsl

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -2,11 +2,98 @@ import React from "react";
 import Carousel from "framer-motion-carousel";
 import "./Programs.css";
 import { programsData } from "../../data/programsData";
-import { image1, image2, image3, kikla, kikla1, kikla2, kikla3, kikla4, kikla5, kikla6, coaches, coaches1, player, coaches2 } from "../../data/kikla_images";
-import RightArrow from '../../assets/rightArrow.png'
+import {
+  image1,
+  image2,
+  image3,
+  kikla,
+  kikla1,
+  kikla2,
+  kikla3,
+  kikla4,
+  kikla5,
+  kikla6,
+  coaches,
+  coaches1,
+  player,
+  coaches2,
+  imageA,
+  imageAcc,
+  imageAcf,
+  imageAer,
+  imageAfv,
+  imageAgd,
+  imageArt,
+  imageAtth,
+  imageAty,
+  imageAvb,
+  imageAz,
+  imageB,
+  imageLa,
+  imageaa,
+  imagec,
+  imagecv,
+  imaged,
+  imageds,
+  imagef,
+  imagefd,
+  imageff,
+  imagegv,
+  imageh,
+  imagelk,
+  imagepl,
+  imager,
+  imagesx,
+  imaget,
+  imagevb,
+  imagew,
+  imaged1,
+} from "../../data/kikla_images";
+import RightArrow from "../../assets/rightArrow.png";
 const Programs = () => {
   const carouselRef = React.useRef();
-  const imagesArray = [image1, image2, image3, kikla, kikla5, kikla6, coaches,]
+  const imagesArray = [
+    image1,
+    image2,
+    image3,
+    kikla,
+    kikla5,
+    kikla6,
+    coaches,
+    player,
+    coaches2,
+    imageA,
+    imageAcc,
+    imageAcf,
+    imageAer,
+    imageAfv,
+    imageAgd,
+    imageArt,
+    imageAtth,
+    imageAty,
+    imageAvb,
+    imageAz,
+    imageB,
+    imageLa,
+    imageaa,
+    imagec,
+    imagecv,
+    imaged,
+    imageds,
+    imagef,
+    imagefd,
+    imageff,
+    imagegv,
+    imageh,
+    imagelk,
+    imagepl,
+    imager,
+    imagesx,
+    imaget,
+    imagevb,
+    imagew,
+    imaged1
+  ];
   return (
     <div className="Programs" id="programs">
       {/* header */}
@@ -30,7 +117,7 @@ const Programs = () => {
           </div>
         ))} */}
 
-        <Carousel ref={carouselRef} autoPlay={false}>
+        <Carousel renderDots={null} ref={carouselRef} autoPlay={false}>
           {imagesArray.map((item, i) => (
             <div className="category">
               <img
@@ -41,38 +128,10 @@ const Programs = () => {
                 key={i}
                 width="100%"
               />
-              <span>program.heading</span>
-              <span>program.details</span>
-
-              <div className="join-now">
-                <span>Join Now</span>
-                <img src={RightArrow} alt="" />
-              </div>
             </div>
           ))}
         </Carousel>
-
-
-        {/* <div className="card">
-               <img
-              draggable="false"
-              src={item}
-              key={i}
-              width="100%"
-              // height="80%"
-              alt=""
-              className="carousel-img"
-            />
-          <div className="card-content">
-            <h2 className="card-title">Card Title</h2>
-            <p className="card-description">
-              This is the description of the card. It can contain some additional information about the content.
-            </p>
-            <button className="card-button">Read More</button>
-          </div>
-        </div> */}
       </div>
-
     </div>
   );
 };
